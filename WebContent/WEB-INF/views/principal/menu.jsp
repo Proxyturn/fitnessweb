245457e1bf0881328fe281cdc4990866b491b53a
Proxyturn <36854170+Proxyturn@users.noreply.github.com>
2018-11-19 14:29:11 -0200

    Add files via upload

---
+++ WebContent/WEB-INF/views/principal/menu.jsp
@@ -0,0 +1,5 @@
+<a href="listaPrincipal">Home</a> | 
+<a href="listaUsuarios">Usuarios</a> | 
+<a href="listaCatalogos">Catalogo</a> | 
+<a href="logout">Sair</a>
+<br /> 
---
+++ src/br/com/cadastro/controller/CatalogoController.java
@@ -0,0 +1,61 @@
+package br.com.cadastro.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.cadastro.dao.CatalogoDAO;
+import br.com.cadastro.model.Catalogo;
+
+@Controller
+public class CatalogoController {
+	
+    @Autowired
+    CatalogoDAO daoCatalogo;
+    
+	@RequestMapping("listaCatalogos")
+    public String lista(Model modelCatalogo) throws ClassNotFoundException {
+        modelCatalogo.addAttribute("catalogos", daoCatalogo.lista());
+        return "catalogo/lista";
+    }
+
+	@RequestMapping("novoCatalogo")
+	public String form() {
+		return "catalogo/formulario";
+	}
+	
+	@RequestMapping("adicionaCatalogo")
+	public String adiciona(@Valid Catalogo catalogo, BindingResult result) throws ClassNotFoundException {
+		if(result.hasFieldErrors("identificacao") || result.hasFieldErrors("descricao")) {
+			 return "catalogo/formulario";
+	     }
+		daoCatalogo.adiciona(catalogo);
+		return "redirect:listaCatalogos";
+	}
+	
+	@RequestMapping("mostraCatalogo")
+    public String mostra(Long id, Model modelCatalogo) throws ClassNotFoundException {
+        modelCatalogo.addAttribute("catalogo", daoCatalogo.buscaPorId(id));
+        return "catalogo/mostra";
+    }
+	
+	@RequestMapping("alteraCatalogo")
+	public String altera(@Valid Catalogo catalogo, BindingResult result) throws ClassNotFoundException {
+		if(result.hasFieldErrors("identificacao") || result.hasFieldErrors("descricao")) {
+			 return "catalogo/mostra";
+	     }
+		daoCatalogo.altera(catalogo);
+        return "redirect:listaCatalogos";
+    }
+	
+    @RequestMapping("removeCatalogo")
+    public String remove(Catalogo catalogo) throws ClassNotFoundException {
+    	daoCatalogo.remove(catalogo);
+        return "redirect:listaCatalogos";
+    }
+    
+}
