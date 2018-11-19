cb3b2219b9342cac073324ce61f985336ff2dd3d
alu201416203 <alu201416203@PC-LAB408-59611.uniritter.local>
2018-11-19 16:11:18 -0200

    asdfdas

--- src/br/com/cadastro/jdbc/ConnectionFactory.java
+++ src/br/com/cadastro/jdbc/ConnectionFactory.java
@@ -6,7 +6,7 @@ public class ConnectionFactory {
 		
 		Class.forName("com.mysql.jdbc.Driver");
         try {
-        	return DriverManager.getConnection("jdbc:mysql://www.anln.com.br/anlncom_cadastro", "anlncom_cadastro", "anlncom_cadastro123456");
+        	return DriverManager.getConnection("jdbc:mysql://www.anln.com.br/anlncom_academia", "anlncom_academia", "@cademia$123456%");
         }catch(SQLException e){
         	System.out.println("Erro de conexao");
 			throw new RuntimeException(e+"\nNAO FOI POSSIVEL CONECTAR NO BANCO DE DADOS");
